a9f23f641a5d5eefc77cc27b58950dca47264c9b
sapslaj <saps.laj@gmail.com>
2015-02-15 19:26:42 -0500

    pry is literally the best thing ever

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'purdytest' # minitest-colorize is broken in minitest version 5
+require 'pry'
 
 require_relative '../lib/youtube-dl.rb'
