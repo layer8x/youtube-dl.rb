ba7ec5b05b87346cc7b45599ec0c910ff6473a04
sapslaj <saps.laj@gmail.com>
2015-02-16 14:58:41 -0500

    Doc'd YoutubeDL::Runner

--- lib/youtube-dl/runner.rb
+++ lib/youtube-dl/runner.rb
@@ -6,30 +6,48 @@ module YoutubeDL
     attr_accessor :options
     attr_accessor :executable_path
 
+    # Command Line runner initializer
+    #
+    # @param url [String] URL to pass to youtube-dl executable
+    # @param options [Hash, Options] options to pass to the executable. Automatically converted to Options if it isn't already
     def initialize(url, options=YoutubeDL::Options.new)
       @url = url
       @options = YoutubeDL::Options.new(options.to_hash)
       @executable_path = 'youtube-dl'
     end
 
+    # Returns Cocaine's runner engine
+    #
+    # @return [CommandLineRunner] backend runner class
     def backend_runner
       Cocaine::CommandLine.runner
     end
 
+    # Sets Cocaine's runner engine
+    #
+    # @param [CommandLineRunner] backend runner class
     def backend_runner=(cocaine_runner)
       Cocaine::CommandLine.runner = cocaine_runner
     end
 
+    # Returns the command string without running anything
+    #
+    # @return [String] command line string
     def to_command
       cocaine_line(options_to_commands).command(@options.store)
     end
     alias_method :command, :to_command
 
+    # Runs the command
     def run
       cocaine_line(options_to_commands).run(@options.store)
     end
 
     private
+
+    # Parses options and converts them to Cocaine's syntax
+    #
+    # @return [String] commands ready to do cocaine
     def options_to_commands
       commands = []
       options.each_paramized_key do |key, paramized_key|
@@ -43,6 +61,10 @@ module YoutubeDL
       commands.join(' ')
     end
 
+    # Helper for doing lines of cocaine (initializing, auto executable stuff, etc)
+    #
+    # @param command [String] command switches to run
+    # @return [Cocaine::CommandLine] initialized Cocaine instance
     def cocaine_line(command)
       Cocaine::CommandLine.new(@executable_path, command)
     end
