f8ecf97b3a2720ff042290a409c61573db6744a7
Enrico Genauck <kontakt@enricogenauck.de>
2015-08-25 09:52:09 +0200

    Ensures test accuracy with specific video format
    It seems that youtube-dl has different format preferences as default
    on different systems.
    This commit sets a specific file format for the download tests to
    have more reliable youtube-dl results and green expectations
    on those hard coded file names and formats.

--- test/youtube-dl/youtube-dl_test.rb
+++ test/youtube-dl/youtube-dl_test.rb
@@ -6,8 +6,8 @@ describe YoutubeDL do
   end
 
   it 'should download videos' do
-    YoutubeDL.get NOPE, output: 'nope'
-    assert File.exist? 'nope.mp4'
+    YoutubeDL.get NOPE, output: 'nope.flv', format: YOUTUBE_DL_FORMAT_FLV
+    assert File.exist? 'nope.flv'
   end
 
   it 'should download multiple videos' do
