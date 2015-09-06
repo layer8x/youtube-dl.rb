89999106d234e5125bd6a443d30c1f3fbb4eccac
sapslaj <saps.laj@gmail.com>
2015-09-06 02:47:07 -0400

    Video things.

--- lib/youtube-dl/video.rb
+++ lib/youtube-dl/video.rb
@@ -2,17 +2,32 @@ module YoutubeDL
   # Video model
   class Video
     class << self
+      # Instantiate a new Video model and download the video
+      #
+      # @param url [String] URL to use and download
+      # @param options [Hash] Options to pass in
+      # @return [YoutubeDL::Video] new Video model
       def download(url, options={})
-        video = new(options.merge(url: url))
+        video = new(url, options)
+        video.download
+        video
       end
 
       alias_method :get, :download
     end
 
-    def initialize(options={})
-      @url = options[:url]
+    # [YoutubeDL::Options] Options access.
+    attr_reader :options
+
+    # Instantiate new model
+    #
+    # @param options [Hash] Options to populate the everything with
+    def initialize(url, options={})
+      @url = url
+      @options = YoutubeDL::Options.new(options)
     end
 
+    # Download the video.
     def download
       YoutubeDL::Runner.new(url, runner_options).run
     end
@@ -21,7 +36,7 @@ module YoutubeDL
     def runner_options
       {
         get_filename: true
-      }
+      }.merge(@options)
     end
   end
 end
