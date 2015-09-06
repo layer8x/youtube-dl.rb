6e8c715ee3a2d192867ae2fc1351648a6cb085c3
sapslaj <saps.laj@gmail.com>
2015-09-06 00:59:58 -0400

    Changed Video test to actually test Video and not just the YoutubeDL module.

--- test/youtube-dl/video_test.rb
+++ test/youtube-dl/video_test.rb
@@ -7,22 +7,22 @@ describe YoutubeDL::Video do
     end
 
     it 'should download videos without options' do
-      YoutubeDL.download TEST_URL
+      YoutubeDL::Video.download TEST_URL
       assert_equal 1, Dir.glob(TEST_GLOB).length
     end
 
     it 'should download videos with options' do
-      YoutubeDL.download TEST_URL, output: TEST_FILENAME, format: TEST_FORMAT
+      YoutubeDL::Video.download TEST_URL, output: TEST_FILENAME, format: TEST_FORMAT
       assert File.exist? TEST_FILENAME
     end
 
     it 'should download multiple videos without options' do
-      YoutubeDL.download [TEST_URL, TEST_URL2]
+      YoutubeDL::Video.download [TEST_URL, TEST_URL2]
       assert_equal 2, Dir.glob(TEST_GLOB).length
     end
 
     it 'should download multiple videos with options' do
-      YoutubeDL.download [TEST_URL, TEST_URL2], output: 'test_%(title)s-%(id)s.%(ext)s'
+      YoutubeDL::Video.download [TEST_URL, TEST_URL2], output: 'test_%(title)s-%(id)s.%(ext)s'
       assert_equal 2, Dir.glob('test_' + TEST_GLOB).length
     end
   end
@@ -33,7 +33,7 @@ describe YoutubeDL::Video do
     end
 
     it 'should download videos, exactly like .download' do
-      YoutubeDL.get TEST_URL
+      YoutubeDL::Video.get TEST_URL
       assert_equal Dir.glob(TEST_GLOB).length, 1
     end
   end
