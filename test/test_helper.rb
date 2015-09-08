e864649f10a5691782047fbf2c02a315777a4e6f
sapslaj <saps.laj@gmail.com>
2015-09-07 21:33:41 -0400

    Added fixture implementation to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,3 +28,11 @@ def remove_downloaded_files
     File.delete(nope)
   end
 end
+
+def fixture(*keys)
+  @fixtures ||= YAML.load(File.read(File.join(File.dirname(__FILE__), 'fixtures.yml')))
+
+  last_path = @fixtures
+  keys.each { |key| last_path = last_path[key] }
+  last_path
+end
