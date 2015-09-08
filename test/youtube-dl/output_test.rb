be92551afe935ad74d1956f4f9b861773f19eb31
sapslaj <saps.laj@gmail.com>
2015-09-07 21:33:54 -0400

    Moved to fixtures in output test.

--- test/youtube-dl/output_test.rb
+++ test/youtube-dl/output_test.rb
@@ -1,38 +1,5 @@
 require_relative '../test_helper'
 
-module OutputFactory
-  extend self
-  extend YoutubeDL::Support
-
-  def download
-    remove_downloaded_files # Make sure there isn't a file there
-    @download ||= cocaine_line(quoted(TEST_URL)).run
-  end
-
-  def download_exists
-    @download_exists ||= lambda do
-      cocaine_line(quoted(TEST_URL)).run # two times to make sure it's there.
-      cocaine_line(quoted(TEST_URL)).run
-    end.call
-  end
-
-  def list_formats
-    @list_formats ||= cocaine_line("--list-formats #{quoted(TEST_URL)}").run
-  end
-end
-
-# For debugging
-# puts
-# puts " --------------- OutputFactory.download --------------- "
-# puts OutputFactory.download
-# puts
-# puts " --------------- OutputFactory.download_exists --------------- "
-# puts OutputFactory.download_exists
-# puts
-# puts " --------------- OutputFactory.list_formats --------------- "
-# puts OutputFactory.list_formats
-# puts
-
 describe YoutubeDL::Output do
   describe '#initialize' do
     it 'should set the output variable' do
@@ -44,7 +11,7 @@ describe YoutubeDL::Output do
 
   describe '#supported_formats' do
     before do
-      @parser = YoutubeDL::Output.new(OutputFactory.list_formats)
+      @parser = YoutubeDL::Output.new(fixture(:output, :list_formats))
     end
 
     it 'should find a match given the correct output' do
@@ -56,37 +23,41 @@ describe YoutubeDL::Output do
     end
 
     it 'should return nil if no match or wrong log format' do
-      bad_parser = YoutubeDL::Output.new(OutputFactory.download)
+      bad_parser = YoutubeDL::Output.new(fixture(:output, :download))
       assert_nil bad_parser.supported_formats
     end
   end
 
   describe '#filename' do
     before do
-      @parser_download = YoutubeDL::Output.new(OutputFactory.download)
-      @parser_download_exists = YoutubeDL::Output.new(OutputFactory.download_exists)
+      @parser_download = YoutubeDL::Output.new(fixture(:output, :download))
+      @parser_download_ffmpeg = YoutubeDL::Output.new(fixture(:output, :download_ffmpeg))
+      @parser_download_exists = YoutubeDL::Output.new(fixture(:output, :download_exists))
     end
 
     it 'should find a match given the correct output' do
       refute_nil @parser_download.filename
+      refute_nil @parser_download_ffmpeg.filename
       refute_nil @parser_download_exists.filename
     end
 
     it 'should find the correct match' do
       assert_equal 'nope.avi-gvdf5n-zI14.mp4', @parser_download.filename
+      assert_equal 'nope.avi-gvdf5n-zI14.mp4', @parser_download_ffmpeg.filename
       assert_equal 'nope.avi-gvdf5n-zI14.mp4', @parser_download_exists.filename
     end
 
     it 'should return nil if no match or wrong log format' do
-      bad_parser = YoutubeDL::Output.new(OutputFactory.list_formats)
+      bad_parser = YoutubeDL::Output.new(fixture(:output, :list_formats))
       assert_nil bad_parser.filename
     end
   end
 
   describe '#already_downloaded?' do
     before do
-      @parser_download = YoutubeDL::Output.new(OutputFactory.download)
-      @parser_download_exists = YoutubeDL::Output.new(OutputFactory.download_exists)
+      @parser_download = YoutubeDL::Output.new(fixture(:output, :download))
+      @parser_download_ffmpeg = YoutubeDL::Output.new(fixture(:output, :download_ffmpeg))
+      @parser_download_exists = YoutubeDL::Output.new(fixture(:output, :download_exists))
     end
 
     it 'should return a truthy value if true' do
@@ -95,6 +66,7 @@ describe YoutubeDL::Output do
 
     it 'should return a falsy value if false' do
       refute @parser_download.already_downloaded?
+      refute @parser_download_ffmpeg.already_downloaded?
     end
   end
 end
