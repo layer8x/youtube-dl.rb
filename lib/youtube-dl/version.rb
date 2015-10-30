95e13a695e4f742763cdbf55881a5c43c1e5acdc
sapslaj <saps.laj@gmail.com>
2015-10-30 06:49:57 -0400

    Bump version and release

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -1,5 +1,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(semver)(pre-release)(binary-version)"
-  VERSION = '0.2.4pre.2015.10.24'
+  VERSION = '0.2.4.2015.10.24'
 end
