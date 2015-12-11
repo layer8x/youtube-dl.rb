83f9c5c60d8112a1e871530ba2b14720e7a4628e
sapslaj <saps.laj@gmail.com>
2015-12-10 21:01:41 -0500

    Intentionally flunk on JRuby to test allowed failures. (reverted from commit c48f269371e0fcc7a100328acebd217e0306bc2b)

--- test/youtube-dl_test.rb
+++ test/youtube-dl_test.rb
@@ -1,10 +1,6 @@
 require_relative './test_helper'
 
 describe YoutubeDL do
-  it do
-    flunk if RUBY_PLATFORM == "java" # Intentionally fail on JRuby to test allowed failures
-  end
-
   describe '.download' do
     after do
       remove_downloaded_files
