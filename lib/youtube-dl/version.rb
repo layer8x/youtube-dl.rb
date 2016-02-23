241368b5c31c68389827d6bed9b59df22c4c9d77
sapslaj <saps.laj@gmail.com>
2016-02-23 02:52:53 -0500

    Remove alpha tag

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -1,5 +1,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.0-alpha.2016.02.22.1'
+  VERSION = '0.3.0.2016.02.22.1'
 end
