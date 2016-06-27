1242d66bc7733f3ccda52965d331d9ab7cf2bfb8
sapslaj <saps.laj@gmail.com>
2016-06-27 01:58:37 -0400

    Bump version to 0.4.0-dev.

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -1,6 +1,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.1.2016.06.25'.freeze
+  VERSION = '0.4.0-dev.2016.06.25'.freeze
 end
-
