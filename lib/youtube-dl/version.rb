8cb6307b78e52f81796d6644bd918bd365ae069d
sapslaj <saps.laj@gmail.com>
2016-08-08 01:21:31 -0400

    Updated binaries to 2016.08.07

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.1.2016.08.06'.freeze
+  VERSION = '0.3.1.2016.08.07'.freeze
 end
